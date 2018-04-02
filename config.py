8bc6f819e032ab00cd99eca42b3a2eb92e031905
leunammejii <theemailaddressishare@gmail.com>
2018-04-02 03:37:30 +0000

    Modify sample output filename

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ IOC Report Configuration File
 # These settings are used to create initial report. File paths should be entered 
 # as the absolute path.
 input_file  = 'sample_urls.txt'
-output_file = 'sample_results.csv'
+output_file = 'sample_full_results.csv'
 
 # VirusTotal configurations
 vt_api_key = ''
