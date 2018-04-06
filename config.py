fe319398244207da9aee126598593e45a26a4e0f
leunammejii <theemailaddressishare@gmail.com>
2018-04-06 23:20:57 +0000

    Add option for text report

--- config.py
+++ config.py
@@ -3,14 +3,15 @@
 IOC Report Configuration File
 """
 
-# These settings are used to create initial report. File paths should be entered 
-# as the absolute path.
-input_file  = 'sample_urls.txt'
-output_file = 'sample_full_results.csv'
-
 # VirusTotal configurations
 vt_api_key = ''
 
 # Hybrid-Analysis configurations
 ha_api_key    = ''
 ha_secret_key = ''
+
+# These settings are used to create initial report. File paths should be entered 
+# as the absolute path.
+input_file      = 'FULLPATH/sample_urls.txt'
+csv_output_file = 'FULLPATH/sample_full_results.csv'
+txt_output_file = 'FULLPATH/sample_flat_results.txt'
