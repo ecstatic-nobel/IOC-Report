084b752e06e1f8b46e84486062210eb5e41cef53
leunammejii <theemailaddressishare@gmail.com>
2018-04-26 15:06:51 +0000

    Modify values

--- config.py
+++ config.py
@@ -12,9 +12,10 @@ ha_secret_key = ''
 
 # These settings are used to create the reports. File paths should be entered 
 # as the absolute path.
-input_file      = 'sample_urls.txt'
-csv_output_file = 'sample_osint_report.csv'
-txt_output_file = 'sample_osint_report.txt'
+input_file       = 'sample_resources.txt'
+csv_output_file  = 'sample_osint_report.csv'
+txt_output_file  = 'sample_osint_report.txt'
+feed_output_file = 'sample_feed_report.csv'
 
 # Web server configurations
 host = '127.0.0.1'
